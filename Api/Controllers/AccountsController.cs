88d4e3a73162a7ee2b7c248828e3775504f53943
sl-yannterraillon <yann.terraillon@groupeseloger.com>
2022-02-17 09:53:47 +0100

    Create Solution & work on activity

---
+++ Api/Controllers/AccountsController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Accounts.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AccountsController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateAccountRequest request)
+    {
+        var command = new CreateAccount.Command(request.Mail, request.Password);
+        var activityFeature = HttpContext.Features.Get<IHttpActivityFeature>();
+
+        var currentActivity = activityFeature?.Activity;
+
+        // TODO : can be done via middleware, but should definitely add the user id to the Activity
+        activityFeature?.Activity
+            .AddBaggage("correlation.id", Guid.NewGuid().ToString());
+
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
+
+public record CreateAccountRequest(string Mail, string Password);
---
+++ Application/Accounts/Commands/Create/CreateAccount.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Application.Accounts.Commands.Create;
+
+public static class CreateAccount
+{
+    public record Response(int Id);
+
+    public record Command(string Email, string Password) :IRequest<Response> ;
+
+    public class Handler : IRequestHandler<Command, Response>
+    {
+        private readonly ILogger<Handler> _logger;
+
+        public Handler(ILogger<CreateAccount.Handler> logger) => _logger = logger;
+
+        public Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("HANDLER");
+
+
+            // TODO : Add Serilog and enricher 
+            if(Activity.Current != null)
+            {
+                foreach (var (key, value) in Activity.Current.Baggage)
+                {
+                    _logger.LogInformation($"{key} : {value}");
+                }
+            }
+
+            return Task.FromResult(new Response(1));
+        }
+    }
+}
